c55f23d9b1f0b5cdbce33d9ed0c17a6705b413f1
Sadra talebpour <sadra.t78@gmail.com>
2021-09-17 13:48:51 +0430

    Before Added InfraStructure

---
+++ AnsarPlast/ShopManagement.Application/ProductCategoryApplication.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using _0_Framework.Application;
+using ShopManagement.Application.Contracts.ProductCategory;
+using ShopManagement.Domain.ProductCategoryAgg;
+
+namespace ShopManagement.Application
+{
+    public class ProductCategoryApplication : IProductCategoryApplication
+    {
+        private readonly IProductCategoryRepository _productCategoryRepository;
+
+        public ProductCategoryApplication(IProductCategoryRepository productCategoryRepository)
+        {
+            _productCategoryRepository = productCategoryRepository;
+        }
+
+        public OperationResult Create(CreateProductCategory command)
+        {
+            var opreation = new OperationResult();
+            if (_productCategoryRepository.Exists(x => x.Name == command.Name)) 
+            {
+                return opreation.Failed("امکان ثبت رکورد تکراری وجود ندارد لطفا مجدد تلاش بفرمایید");
+            }
+
+            var slug = command.Slug.Slugify();
+            var productCategory = new ProductCategory(command.Name, command.Description,
+                command.Picture, command.PictureAlt,
+                command.PictureTitle, command.Keyword,
+                command.MetaDescription, slug);
+            _productCategoryRepository.Create(productCategory);
+            _productCategoryRepository.SaveChanges();
+            return opreation.Succedded();
+        }
+
+        public OperationResult Edit(EditProductCategory command)
+        {
+            var opreation = new OperationResult();
+            var productCategory = _productCategoryRepository.Get(command.Id);
+            if (productCategory == null)
+            {
+                return opreation.Failed("رکورد با اطلاعات درخواست شده یافت نشد لطفا مجد تلاش بفرمایید");
+            }
+
+            if (_productCategoryRepository.Exists(x => x.Name == command.Name && x.Id != command.Id))
+            {
+                return opreation.Failed("امکان ثبت رکورد تکراری وجود ندارد لطفا مجدد تلاش بفرمایید");
+
+            }
+            var slug = command.Slug.Slugify();
+            productCategory.Edit(command.Name,command.Description, command.Picture, command.PictureAlt, command.PictureTitle, command.Keyword, command.MetaDescription, slug);
+            _productCategoryRepository.SaveChanges();
+            return opreation.Succedded();
+        }
+
+        public EditProductCategory GetDetails(long id)
+        {
+            return _productCategoryRepository.GetDetails(id);
+        }
+
+        public List<ProductCategoryViewModel> Search(ProductCategorySearchModel searchModel)
+        {
+            return _productCategoryRepository.Search(searchModel);
+        }
+    }
+}
---
+++ AnsarPlast/ShopManagement.Domain/ProductCategoryAgg/IProductCategoryRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using ShopManagement.Application.Contracts.ProductCategory;
+
+namespace ShopManagement.Domain.ProductCategoryAgg
+{
+    public interface IProductCategoryRepository
+    {
+        void Create(ProductCategory entity);
+        ProductCategory Get(long id);
+        List<ProductCategory> GetAll();
+        bool Exists(Expression<Func<ProductCategory,bool>> expression);
+        void SaveChanges();
+        EditProductCategory GetDetails(long id);
+        List<ProductCategoryViewModel> Search(ProductCategorySearchModel searchModel);
+    }
+}
